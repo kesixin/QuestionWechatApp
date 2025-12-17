5963f6a9f3b64b7da94d3bba2fbd49dea58f8279
智汇考题 <462369233@qq.com>
2025-12-17 09:47:52 +0800

    Update util.js

--- utils/util.js
+++ utils/util.js
@@ -8,7 +8,6 @@ const formatTime = date => {
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':');
 }
-
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
